5ea2c69281559580009b57ad040b22ade9086dc9
ashraf56 <ashrafulfahim07@gmail.com>
2024-07-10 16:47:31 +0600

    product route created

--- dist/app/module/products/products.route.js
+++ dist/app/module/products/products.route.js
@@ -1,7 +1,13 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.ProductRouter = void 0;
 const express_1 = require("express");
+const validationRequest_1 = __importDefault(require("../../middleware/validationRequest"));
+const products_validation_1 = require("./products.validation");
+const products_controller_1 = require("./products.controller");
 const router = (0, express_1.Router)();
-router.post('/create-product');
+router.post('/create-product', (0, validationRequest_1.default)(products_validation_1.ProductValidation.createProductValidation), products_controller_1.ProductCOntroller.createProductController);
 exports.ProductRouter = router;
--- src/app/module/products/products.route.ts
+++ src/app/module/products/products.route.ts
@@ -1,10 +1,14 @@
 import { Router } from "express";
+import validationRequest from "../../middleware/validationRequest";
+import { ProductValidation } from "./products.validation";
+import { ProductCOntroller } from "./products.controller";
 
 
 const router = Router();
 
 
-router.post('/create-product')
+router.post('/create-product', validationRequest(ProductValidation.createProductValidation), ProductCOntroller.createProductController)
+
 
 
 
