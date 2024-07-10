67b0b5f86b3ad5b81eb38999f1b83f53224c00ed
ashraf56 <ashrafulfahim07@gmail.com>
2024-07-10 16:55:29 +0600

    get all product service created

--- dist/app/module/products/products.service.js
+++ dist/app/module/products/products.service.js
@@ -13,11 +13,20 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.ProductServices = void 0;
+const throwErrorHandller_1 = __importDefault(require("../../utills/throwErrorHandller"));
 const products_model_1 = __importDefault(require("./products.model"));
 const createProductDB = (payload) => __awaiter(void 0, void 0, void 0, function* () {
+    const existingProduct = yield products_model_1.default.findOne({ name: payload.name });
+    if (existingProduct) {
+        (0, throwErrorHandller_1.default)('Product already created');
+    }
     const res = yield products_model_1.default.create(payload);
     return res;
 });
+const getAllProductfromDB = () => __awaiter(void 0, void 0, void 0, function* () {
+    const res = yield products_model_1.default.find();
+    return res;
+});
 exports.ProductServices = {
-    createProductDB
+    createProductDB, getAllProductfromDB
 };
--- src/app/module/products/products.service.ts
+++ src/app/module/products/products.service.ts
@@ -1,12 +1,23 @@
+import throwErrorHandller from "../../utills/throwErrorHandller"
 import { ProductInterface } from "./products.interface"
 import Products from "./products.model"
 
 
 
-const createProductDB = async(payload:ProductInterface)=>{
+const createProductDB = async (payload: ProductInterface) => {
+    const existingProduct = await Products.findOne({ name: payload.name })
+    if (existingProduct) {
+        throwErrorHandller('Product already created')
+    }
+    const res = await Products.create(payload)
+    return res
 
-const res = await Products.create(payload)
-return res
+
+}
+const getAllProductfromDB = async () => {
+
+    const res = await Products.find()
+    return res
 
 
 }
@@ -14,19 +25,6 @@ return res
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
 export const ProductServices = {
-    createProductDB
+    createProductDB, getAllProductfromDB
 }
